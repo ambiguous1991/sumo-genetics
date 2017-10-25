2008a27124642f23623525f68515b67fcbb922e4
Kuba <satanicus1991@gmail.com>
2017-10-25 18:51:54 +0200

    Changes to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import log.Log;
 
 public class Main {
     public static void main(String args[]){
-        int dataSwitch = 1;
+        int dataSwitch = 3;
         Log.init("Shuffle",dataSwitch);
         new Thread(new Population(50, 1000, dataSwitch)).start();
     }
