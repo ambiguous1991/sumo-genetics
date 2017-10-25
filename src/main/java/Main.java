39d21927d13512f3d5a77bfecfb06d749f96b942
Kuba <satanicus1991@gmail.com>
2017-10-25 18:10:18 +0200

    Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import log.Log;
 
 public class Main {
     public static void main(String args[]){
-        int dataSwitch = 3;
-        Log.init("EqualSplice",dataSwitch);
+        int dataSwitch = 1;
+        Log.init("Random",dataSwitch);
         new Thread(new Population(30000, 1000, dataSwitch)).start();
     }
 }
